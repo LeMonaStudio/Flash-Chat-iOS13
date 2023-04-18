33eee2690afd5246f7d2cdc956a38487ce03e54f
Musa Lawrence Ilemona <musa.lawrence93@gmail.com>
2023-04-18 15:05:31 +0100

    Created a .xib file for a custom UITableCell and tested display of the TableView with dummy messages.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,8 +14,19 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hello, this is 1@2.com. How are you doing?"),
+        Message(sender: "a@b.com", body: "Hey, I'm fine and how are you doing too?"),
+        Message(sender: "1@2.com", body: "I'm fine as well, thank you for asking.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
         title = K.appName
         navigationItem.hidesBackButton = true
     }
@@ -31,5 +42,35 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+//        if #available(iOS 14.0, *) {
+//            var contentConfig = cell.defaultContentConfiguration()
+//            contentConfig.text = messages[indexPath.row].body
+//            cell.contentConfiguration = contentConfig
+//        } else {
+//            // Fallback on earlier versions
+//            cell.textLabel?.text = messages[indexPath.row].body
+//        }
+        
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
+
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(messages[indexPath.row].body)
     }
 }
